f620646526049d3780a97152df1dee5c2bfda605
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2019-10-17 20:07:54 +0530

    Add files via upload

---
+++ website-code-02-01.ps1
@@ -0,0 +1,11 @@
+# Set execution policy
+Set-ExecutionPolicy Unrestricted -Force
+New-Item -ItemType directory -Path 'C:\temp'
+
+# Install IIS and Web Management Tools.
+Import-Module ServerManager
+install-windowsfeature web-server, web-webserver -IncludeAllSubFeature
+install-windowsfeature web-mgmt-tools
+
+# Download the files for our web application.
+Set-Location -Path C:\inetpub\wwwroot
