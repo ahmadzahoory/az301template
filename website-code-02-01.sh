463895830380001c6fc4331db50fa5afb41b30ac
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2019-10-17 20:07:24 +0530

    Delete website-code-02-01.sh

--- website-code-02-01.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo yum -y update
-sudo yum install -y httpd
-sudo service httpd start
-sudo systemctl enable httpd.service
-sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
-sudo firewall-cmd --reload
