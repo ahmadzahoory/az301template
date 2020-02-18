6e85226c20744a0b1a33bf6cd60a930be24c09bd
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2020-02-19 01:45:40 +0530

    Update az-301-02-02-script.ps1

--- az-301-02-02-script.ps1
+++ az-301-02-02-script.ps1
@@ -2,14 +2,8 @@
 Set-ExecutionPolicy Unrestricted -Force
 New-Item -ItemType directory -Path 'C:\temp'
 
-# Download website code from Github
-mkdir c:\net-core
-
-# Download .Net Core Windows Hosting
-Invoke-WebRequest https://download.visualstudio.microsoft.com/download/pr/d08902cf-87b0-4bfe-b25c-627ff77b8802/8293c56094f50ba8c4ed703aff95c325/dotnet-hosting-2.1.14-win.exe -OutFile c:\net-core\dotnet-hosting-2.1.14-win.exe
-
-# Install .Net Core Windows Hosting
-Start-Process -Wait -FilePath "c:\net-core\dotnet-hosting-2.1.14-win.exe" -ArgumentList "/S" -PassThru -NoNewWindow
+# Set TLS
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 
 # Install IIS and Web Management Tools.
 Import-Module ServerManager
@@ -28,5 +22,23 @@ $destination.copyHere($zipfile.items())
 
 # Create the web app in IIS
 New-WebApplication -Name netapp -PhysicalPath c:\inetpub\wwwroot\cosmos-db-website -Site "Default Web Site" -force
+
+# Download website code from Github
+mkdir c:\net-core
+
+# Download .Net Core Windows Hosting
+Invoke-WebRequest https://bitbucket.org/ahmadzahoory/az301template/downloads/Az-301-02-02-Dotnet-Hosting.zip -OutFile c:\net-core\Az-301-02-02-Dotnet-Hosting.zip
+
+# Unzip .zip file
+Expand-Archive c:\net-core\Az-301-02-02-Dotnet-Hosting.zip -DestinationPath c:\net-core\
+
+# Install .Net Core Windows Hosting
+Start-Process -Wait -FilePath "c:\net-core\dotnet-hosting-2.1.12-win.exe" -ArgumentList "/S" -PassThru -NoNewWindow
+
+# Download SQL Server Management Studio (SSMS)
+Invoke-WebRequest https://aka.ms/ssmsfullsetup -OutFile c:\net-core\ssms-setup-enu.exe
+
+# Install SQL Server Management Studio (SSMS)
+Start-Process -Wait -FilePath "c:\net-core\ssms-setup-enu.exe" -ArgumentList "/S" -PassThru -NoNewWindow
 
 #End
