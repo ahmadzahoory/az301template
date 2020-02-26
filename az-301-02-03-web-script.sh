1769a4dac67ecc2f7caa508b9e840b112f5f093b
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2020-02-26 19:35:55 +0530

    Update az-301-02-03-web-script.sh

--- az-301-02-03-web-script.sh
+++ az-301-02-03-web-script.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+yum -y update
+yum install -y httpd
+yum install -y unzip
 yum install -y wget
-wget https://repo.mysql.com/mysql80-community-release-el8-1.noarch.rpm
-yum localinstall mysql80-community-release-el8-1.noarch.rpm
-yum install mysql-server
-systemctl start mysqld
-systemctl enable mysqld
+service httpd start
+systemctl enable httpd.service
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --reload
+cd /var/www/html/
+wget https://raw.githubusercontent.com/ahmadzahoory/az301template/master/website-code-M02-03.zip
+unzip website-code-M02-03.zip
